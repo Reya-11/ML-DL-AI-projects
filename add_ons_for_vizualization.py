1d28128864693485f866171f21856c64697222eb
Reya <97894972+Reya-11@users.noreply.github.com>
2023-08-28 11:21:54 +0530

    Create add_ons_for_vizualization.py

---
+++ add_ons_for_vizualization.py
@@ -0,0 +1,11 @@
+# Take a single image, and remove the color dimension by reshaping
+for image, label in test_dataset.take(1):
+  break
+image = image.numpy().reshape((28,28))
+
+# Plot the image - voila a piece of fashion clothing
+plt.figure()
+plt.imshow(image, cmap=plt.cm.binary)
+plt.colorbar()
+plt.grid(False)
+plt.show()
